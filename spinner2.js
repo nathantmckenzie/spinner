3f716069679440cc8548e2359f5dd55caf2c41ab
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-04 08:55:47 -0800

    spinner animation

---
+++ spinner2.js
@@ -0,0 +1,13 @@
+let arr = ["|","/","-","\\","|"];
+let delay = 100;
+for (const char of arr) {
+setTimeout(() => {
+    process.stdout.write('\r' + char + '     ');
+  }, delay += 200);
+}
+
+setTimeout(() => {
+  console.log();
+}, arr.length * 250);
+
+
